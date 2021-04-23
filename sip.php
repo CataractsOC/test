f526a3b57d9c945e555607456b621f9a0ef4a0ad
phone <username2046@gmail.com>
2021-04-23 03:50:16 -0400

    Create sip.php

---
+++ sip.php
@@ -0,0 +1,9 @@
+<Response>
+<Dial>
+<Sip>
+<Uri username="twilio1" password="drawtheowl">1337@67.83.28.43</Uri>
+<Uri username="twilio2" password="drawtheowl">1337@67.83.28.43</Uri>
+<Uri username="twilio3" password="drawtheowl">1337@67.83.28.43</Uri>
+</Sip>
+</Dial>
+</Response>
